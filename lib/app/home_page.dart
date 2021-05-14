e2242f7db927963acee7868761bc8de3bad2a3c8
gags-21 <gagan.bhirangi123@gmail.com>
2021-05-14 23:25:03 +0530

    Authentication Flow Implemented

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,17 +1,14 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:time_tracker/services/auth.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key key,@required this.onSignOut,@required this.auth}) : super(key: key);
+  const HomePage({Key key, @required this.auth}) : super(key: key);
 
-  final VoidCallback onSignOut;
   final AuthBase auth;
 
   Future<void> _signOut() async{
     try {
       await auth.signOut();
-      onSignOut();
     } catch(e){
       print(e.toString());
     }
--- lib/app/landing_page.dart
+++ lib/app/landing_page.dart
@@ -4,42 +4,32 @@ import 'package:time_tracker/app/home_page.dart';
 import 'package:time_tracker/app/sign_in/sign_in_page.dart';
 import 'package:time_tracker/services/auth.dart';
 
-class LandingPage extends StatefulWidget {
-  const LandingPage({Key key,@required this.auth}) : super(key: key);
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key key, @required this.auth}) : super(key: key);
   final AuthBase auth;
 
   @override
-  _LandingPageState createState() => _LandingPageState();
-}
-
-class _LandingPageState extends State<LandingPage> {
-  User _user;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _updateUser(widget.auth.currentUser);
-  }
-
-  void _updateUser(User user) {
-    setState(() {
-      _user = user;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    if (_user == null) {
-      return SignInPage(
-        auth: widget.auth,
-        onSignIn: _updateUser,
-      );
-    } else {
-      return HomePage(
-        auth: widget.auth,
-        onSignOut: () => _updateUser(null),
-      );
-    }
+    return StreamBuilder<User>(
+        stream: auth.authStateChanges(),
+        builder: (context, snapShot) {
+          if (snapShot.connectionState == ConnectionState.active) {
+            final User user = snapShot.data;
+            if (user == null) {
+              return SignInPage(
+                auth: auth,
+              );
+            } else {
+              return HomePage(
+                auth: auth,
+              );
+            }
+          }
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        });
   }
 }
